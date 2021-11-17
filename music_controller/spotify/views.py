f8296b6f3a6adcdba596d24932b9b76fac476ab9
cj6046 <cdjones6046@gmail.com>
2021-11-17 11:05:43 -0500

    Created spotify/urls.py and Updated spotify/view.py
    In urls, I created a path from the main app to the spotify app and from there to the authentication api view. Then created that api view in spotify/views. credentials.py was also added to take advantage of environment variables for spotify api codes.

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .credentials import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+from rest_framework.views import APIView
+from requests import Request, post
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+class AuthUrl(APIView):
+    def get(self, request, format=None):
+        scope = 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
+        auth_url = Request('GET', 'https://accounts.spotify.com/authorize', params={
+            'scope': scope,
+            'response_type': 'code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+        }).prepare().url
+
+        return Response({'url' : auth_url}, status=status.HTTP_200_OK)
+
